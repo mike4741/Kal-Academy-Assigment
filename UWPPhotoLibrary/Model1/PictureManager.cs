f9598ad061dd4ecc9872afc37f5e38e1e875af23
misurac <rmisurac@gmail.com>
2021-02-18 08:46:55 -0600

    I have created picture filtering (by category) within PictureManager.cs

--- UWPPhotoLibrary/Model1/PictureManager.cs
+++ UWPPhotoLibrary/Model1/PictureManager.cs
@@ -11,68 +11,53 @@ namespace UWPPhotoLibrary.Model1
    public static class PictureManager
     {
 
-        public static List<Picture> SelectedCategoryPicture(List<Picture> picture, PictureCategory category, string name)
+        public static void GetAllPicture(ObservableCollection<Picture> pictures)
         {
-            var allPic = getAllPicture();
-            var filpic = allPic.Where(pic1 => pic1.Category == category ).ToList();
-            picture.Clear();
-            filpic.ForEach(pic => picture.Add(pic));
-            return picture;
+            var allpics = getAllPicture();
+            pictures.Clear();
+            allpics.ForEach (pic => pictures.Add(pic)) ;
 
         }
-        public static List<Picture> SelectedPicture(List<Picture> picture , PictureCategory category ,string name)
-        {
-            var allPic = getAllPicture();
-            var filpic = allPic.Where(pic1 => pic1.Category == category && pic1.Name == name).ToList();
-            picture.Clear();
-            filpic.ForEach(pic => picture.Add(pic));
-            return picture;
 
+        public static void GetPicturesByCategory(ObservableCollection<Picture> pictures, PictureCategory category)
+        {
+            var allpics = getAllPicture();
+            var filteredPictures = allpics.Where(pic => pic.Category == category).ToList();
+            pictures.Clear();
+            filteredPictures.ForEach(pic => pictures.Add(pic));
         }
-        public static void GetAllPicture(ObservableCollection<Picture> picture)
+        private  static List<Picture> getAllPicture()
         {
-            var allpic = getAllPicture();
-            picture.Clear();
-            //foreach(var pic in allpic)
-            //{
-            //    picture.Add(pic);
-            //}
-             allpic.ForEach (pic => picture.Add(pic)) ;
-            
+            var pictures = new List<Picture>();
+            pictures.Add(new Picture("Cooking1", PictureCategory.Cooking));
+            pictures.Add(new Picture("Cooking2", PictureCategory.Cooking));
+            pictures.Add(new Picture("Cooking3", PictureCategory.Cooking));
+            pictures.Add(new Picture("Cooking4", PictureCategory.Cooking));
+            pictures.Add(new Picture("Cooking5", PictureCategory.Cooking));
 
-        }
-        private  static List<Picture>   getAllPicture()
-        {
-            var picture = new List<Picture>();
-            picture.Add(new Picture("Cooking1", PictureCategory.Cooking));
-            picture.Add(new Picture("Cooking2", PictureCategory.Cooking));
-            picture.Add(new Picture("Cooking3", PictureCategory.Cooking));
-            picture.Add(new Picture("Cooking4", PictureCategory.Cooking));
-            picture.Add(new Picture("Cooking5", PictureCategory.Cooking));
-
-            picture.Add(new Picture("Family1", PictureCategory.Family));
-            picture.Add(new Picture("Family2", PictureCategory.Family));
-            picture.Add(new Picture("Family3", PictureCategory.Family));
-            picture.Add(new Picture("Family4", PictureCategory.Family));
-            picture.Add(new Picture("Family5", PictureCategory.Family));
+            pictures.Add(new Picture("Family1", PictureCategory.Family));
+            pictures.Add(new Picture("Family2", PictureCategory.Family));
+            pictures.Add(new Picture("Family3", PictureCategory.Family));
+            pictures.Add(new Picture("Family4", PictureCategory.Family));
+            pictures.Add(new Picture("Family5", PictureCategory.Family));
 
 
-            picture.Add(new Picture("Holiday1", PictureCategory.Holidays));
-            picture.Add(new Picture("Holiday2", PictureCategory.Holidays));
-            picture.Add(new Picture("Holiday3", PictureCategory.Holidays));
-            picture.Add(new Picture("Holiday4", PictureCategory.Holidays));
-            picture.Add(new Picture("Holiday5", PictureCategory.Holidays));
+            pictures.Add(new Picture("Holiday1", PictureCategory.Holidays));
+            pictures.Add(new Picture("Holiday2", PictureCategory.Holidays));
+            pictures.Add(new Picture("Holiday3", PictureCategory.Holidays));
+            pictures.Add(new Picture("Holiday4", PictureCategory.Holidays));
+            pictures.Add(new Picture("Holiday5", PictureCategory.Holidays));
 
 
-            picture.Add(new Picture("Vacation1", PictureCategory.Vacations));
-            picture.Add(new Picture("Vacation2", PictureCategory.Vacations));
-            picture.Add(new Picture("Vacation3", PictureCategory.Vacations));
-            picture.Add(new Picture("Vacation4", PictureCategory.Vacations));
-            picture.Add(new Picture("Vacation5", PictureCategory.Vacations));
+            pictures.Add(new Picture("Vacation1", PictureCategory.Vacations));
+            pictures.Add(new Picture("Vacation2", PictureCategory.Vacations));
+            pictures.Add(new Picture("Vacation3", PictureCategory.Vacations));
+            pictures.Add(new Picture("Vacation4", PictureCategory.Vacations));
+            pictures.Add(new Picture("Vacation5", PictureCategory.Vacations));
 
 
 
-            return picture;
+            return pictures;
         }
 
       
